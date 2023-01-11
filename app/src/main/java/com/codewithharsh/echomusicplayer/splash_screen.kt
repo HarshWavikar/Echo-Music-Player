b03bd9b3952cc887ba6ba075917adaa58421373c
HarshWavikar <harshwavikar17@gmail.com>
2023-01-11 21:36:42 +0530

    Initial Commit

---
+++ app/src/main/java/com/codewithharsh/echomusicplayer/splash_screen.kt
@@ -0,0 +1,20 @@
+package com.codewithharsh.echomusicplayer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+
+class splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this@splash_screen, MainActivity::class.java)
+            startActivity(intent)
+        },1000)
+    }
+}
