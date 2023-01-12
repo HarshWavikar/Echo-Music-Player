185390396dce56af3b05f978785de5ebe47227a7
HarshWavikar <harshwavikar17@gmail.com>
2023-01-12 15:55:14 +0530

    Initialized all xml views and fun setUpToolBar()

--- app/src/main/java/com/codewithharsh/echomusicplayer/MainActivity.kt
+++ app/src/main/java/com/codewithharsh/echomusicplayer/MainActivity.kt
@@ -2,10 +2,37 @@ package com.codewithharsh.echomusicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout : DrawerLayout
+    private lateinit var coordinatorLayout: CoordinatorLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var frame: FrameLayout
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        coordinatorLayout = findViewById(R.id.coordinatorLayout)
+        toolbar = findViewById(R.id.toolbar)
+        frame = findViewById(R.id.frame)
+        navigationView= findViewById(R.id.navigationView)
+        setUpToolBar()
+
+    }
+
+    fun setUpToolBar(){
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "All Songs"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
