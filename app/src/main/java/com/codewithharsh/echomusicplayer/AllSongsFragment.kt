99f6a61ad05c4f840eb12f3fd9d392ef5f80fefb
HarshWavikar <harshwavikar17@gmail.com>
2023-01-12 20:18:45 +0530

    fragments for all songs, favourites, settings, about us created

---
+++ app/src/main/java/com/codewithharsh/echomusicplayer/AllSongsFragment.kt
@@ -0,0 +1,22 @@
+package com.codewithharsh.echomusicplayer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AllSongsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_all_songs, container, false)
+        return view
+    }
+
+
+}
